57220f0fb30a831289c43ccfcf6b344a5cfa5ba8
Max Pokidaylo <maxim.p9@gmail.com>
2020-10-12 17:26:33 +0300

    add login component, add login route

--- section19-project-getting-started/src/app/app-routing.module.ts
+++ section19-project-getting-started/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { CheckOutComponent } from './check-out/check-out.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { OrderSuccessComponent } from './order-success/order-success.component';
 import { ProductsComponent } from './products/products.component';
@@ -18,7 +19,8 @@ const routes: Routes = [
   { path: 'order-success', component: OrderSuccessComponent },
   { path: 'my-orders', component: MyOrdersComponent },
   { path: 'admin/orders', component: AdminOrdersComponent },
-  { path: 'admin/products', component: AdminProductsComponent }
+  { path: 'admin/products', component: AdminProductsComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
